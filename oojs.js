bfcd8181f4426547bc89959571709b3553837cc3
hgergosztg-lang <hgergosztg@gmail.com>
2026-04-12 22:55:15 +0200

    Update oojs.js

--- oojs.js
+++ oojs.js
@@ -1,44 +1,60 @@
 class Task {
-    constructor(title) {
+    constructor(title, date = new Date()) {
         this.title = title;
+        this.date = date;
+        this.done = false;
     }
 
     render() {
-        const div = document.createElement("div");
-        div.className = "card";
+        const card = document.createElement("div");
+        card.className = "card";
 
-        div.innerHTML = `
-            <p>${this.title}</p>
-        `;
+        const text = document.createElement("p");
+        text.innerHTML = `<b>${this.title}</b><br><small>${this.date.toLocaleString()}</small>`;
 
-        return div;
+        card.appendChild(text);
+
+        return card;
     }
 }
-
-class DeletableTask extends Task {
-    constructor(title, onDelete) {
-        super(title); // super használata kötelező
+class SmartTask extends Task {
+    constructor(title, onUpdate, onDelete) {
+        super(title); // super kötelező
+        this.onUpdate = onUpdate;
         this.onDelete = onDelete;
     }
 
     render() {
-        const div = super.render();
+        const card = super.render();
 
-        const btn = document.createElement("button");
-        btn.innerText = "Törlés";
+        const completeBtn = document.createElement("button");
+        completeBtn.innerText = "Kész";
+        completeBtn.className = "btn complete";
 
-        btn.onclick = () => {
-            this.onDelete(div);
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "Törlés";
+        deleteBtn.className = "btn delete";
+
+        completeBtn.onclick = () => {
+            card.classList.toggle("done");
+            this.onUpdate(this);
         };
 
-        div.appendChild(btn);
+        deleteBtn.onclick = () => {
+            card.remove();
+            this.onDelete(this);
+        };
 
-        return div;
+        card.appendChild(completeBtn);
+        card.appendChild(deleteBtn);
+
+        return card;
     }
 }
 class App {
     constructor() {
-        this.list = document.getElementById("list");
+        this.board = document.getElementById("board");
+        this.tasks = [];
     }
 
     addTask() {
@@ -47,13 +63,18 @@ class App {
 
         if (!value) return;
 
-        const task = new DeletableTask(value, (element) => {
-            element.remove();
-        });
+        const task = new SmartTask(
+            value,
+            () => {},
+            () => {}
+        );
 
-        this.list.appendChild(task.render());
+        this.tasks.push(task);
+
+        this.board.appendChild(task.render());
 
         input.value = "";
     }
 }
+
 const app = new App();
